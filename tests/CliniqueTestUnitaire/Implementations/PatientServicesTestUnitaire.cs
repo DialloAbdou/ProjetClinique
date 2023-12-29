c958971e9611cc1c89cdec4c8576b6fbd0760f0c
DialloAbdou <abdoudiallo3826@yahoo.fr>
2023-12-29 19:29:11 +0100

    ajout des test Unitaires

---
+++ tests/CliniqueTestUnitaire/Implementations/PatientServicesTestUnitaire.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CliniqueTestUnitaire.Implementations
+{
+    public class PatientServicesTestUnitaire
+    {
+
+        public void CreatePatient_Should_Be_Return_New_Patient()
+        {
+
+        }
+    }
+}
